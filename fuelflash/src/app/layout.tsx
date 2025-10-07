75e122315101db7af2283938b5c347b8dd1982eb
kezaqueen <rutagengwakezaqueen@gmail.com>
2025-10-07 11:29:11 +0300

    added footer files

--- fuelflash/src/app/layout.tsx
+++ fuelflash/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
